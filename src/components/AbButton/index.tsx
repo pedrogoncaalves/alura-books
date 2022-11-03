4097106db5040990e8a6b9e6fd84f8a4ecdc506f
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-11-03 16:05:15 -0300

    creating styles

--- src/components/AbButton/index.tsx
+++ src/components/AbButton/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import * as C from './styles'
 
 
 export const AbButton = () => {
     return(
-        <button>
-            ...
-        </button>
+        <C.styledButton>
+            Clique
+        </C.styledButton>
     )
 } 
---
+++ src/components/AbButton/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+
+export const styledButton = styled.button`
+background: #EB9B00;
+padding: 16px 32px;
+border: 2px solid #EB9B00;
+color: #FFFFFF;
+font-size: 20px;
+cursor: pointer;
+&:hover {
+    background: #B87900;
+    border: #B87900;
+}
+`
